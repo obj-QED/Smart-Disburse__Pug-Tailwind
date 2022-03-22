36249d7af452267d64cdad241174a4b7d12a9c51
obj-QED <obj.qed@gmail.com>
2022-03-22 09:35:06 +0200

    update new design

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,46 +1,55 @@
 // Определяем поддержку Webp
 function testWebP(callback) {
-    var webP = new Image();
-    webP.onload = webP.onerror = function () {
-        callback(webP.height == 2);
-    };
-    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
 
 testWebP(function (support) {
-    if (support == true) {
-        document.querySelector('body').classList.add('webp');
-    } else {
-        document.querySelector('body').classList.add('no-webp');
-    }
+  if (support == true) {
+    document.querySelector("body").classList.add("webp");
+  } else {
+    document.querySelector("body").classList.add("no-webp");
+  }
 });
 
-$(document).ready(function() {
-    // Modal Open
-    $('.message').on('click', function() {
-        $('.modal-message').show();
-    });
-    $('.expiration').on('click', function() {
-        $('.modal-expiration').show();
-    });
-    $('.full-details').on('click', function() {
-        $('.modal-full-details').show();
-    });
-    $('.js-open-modal').on('click', function(){
-        $('.modal').show();
-    });
-    // Close modal
-    $('.modal .close').on('click', function(){
-        $(this).parents('.modal').hide();
-    })
-    // Accordion Menu
-    $('.menu .nav-accordion .nav-accordion__btn').on('click', function(){
-        $(this).toggleClass('transform rotate-180');
-        $(this).parents('.menu').find('.menu-nav .item span.text-sm').toggleClass('hidden');
-        $(this).parents('.menu').find('.copyright span, .copyright a').toggleClass('hidden');
-        $(this).parents('.menu').find('.sticky').toggleClass('min-w-220p');
-        $(this).parents('.menu').find('.menu-nav').toggleClass('flex flex-col items-center justify-center');
-        $(this).parents('.menu').toggleClass('md:w-2/12 w-auto md:w-4/12');
-        $(this).parents('.step').find('.content').toggleClass('md:w-8/12 w-full px-36 md:py-10 pt-9 px-4 pb-4 md:p-10 lg:p-14 lg:py-14');
-    });
+$(document).ready(function () {
+  // Modal Open
+  $(".message").on("click", function () {
+    $(".modal-message").show();
+  });
+  $(".expiration").on("click", function () {
+    $(".modal-expiration").show();
+  });
+  $(".full-details").on("click", function () {
+    $(".modal-full-details").show();
+  });
+  $(".js-open-modal").on("click", function () {
+    $(".modal").show();
+  });
+  // Close modal
+  $(".modal .close").on("click", function () {
+    $(this).parents(".modal").hide();
+  });
+  // Accordion Menu
+  $(".menu .nav-accordion .nav-accordion__btn").on("click", function () {
+    $(this).toggleClass("transform rotate-180");
+    $(this).parents(".menu").find(".menu-nav .item span.text-sm").toggleClass("hidden");
+    $(this).parents(".menu").find(".copyright span, .copyright a").toggleClass("hidden");
+    $(this).parents(".menu").find(".sticky").toggleClass("min-w-220p w-full items-center");
+    $(this).parents(".menu").find(".menu-nav").toggleClass("flex flex-col items-center justify-center");
+    $(this).parents(".menu").toggleClass("md:w-2/12 w-auto md:w-4/12");
+    $(this).parents(".step").find(".content").toggleClass("md:w-8/12 w-full px-36 md:py-10 pt-9 px-4 pb-4 md:p-10 lg:p-14 lg:py-14");
+
+    $(this).parent().toggleClass("justify-end justify-center");
+    $(this).parents(".menu").find(".terms-link").toggleClass("flex-wrap order-2");
+    $(this).parents(".menu").find(".js-switch-language").toggleClass("order-3");
+    $(this).parents(".menu").find(".js-powered").toggleClass("order-1");
+    $(this).parents(".menu").find(".js-footer-separator").toggleClass("hidden");
+    $(this).parents(".menu").find(".js-footer-transcard").toggleClass("hidden");
+    $(this).parents(".menu").find(".js-footer-powered").toggleClass("hidden");
+    $(this).parents(".menu").find(".js-foooter-container").toggleClass("flex-col-reverse");
+  });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -2,35 +2,31 @@ let project_folder = "dist";
 let source_folder = "#src";
 
 let path = {
-    build: {
-        html: project_folder + "/",
-        css: project_folder + "/SMART-Disburse/css/",
-        js: project_folder + "/SMART-Disburse/js/",
-        img: project_folder + "/SMART-Disburse/img/",
-        fonts: project_folder + "/SMART-Disburse/fonts/",
-    },
-    src: {
-        html: [source_folder + "/views/**/*.pug", "!" + source_folder + "/views/_*.pug", "!" + source_folder + "/views/**/#**/*.pug"],
-        css: source_folder + "/stylus/*.styl",
-        js: source_folder + "/js/*.js",
-        img: source_folder + "/img/**/*.+(png|jpg|gif|ico|svg|webp)",
-        fonts: source_folder + "/fonts/*.ttf",
-    },
-    watch: {
-        html: source_folder + "/**/**/*.pug",
-        css: source_folder + "/stylus/**/*.styl",
-        js: source_folder + "/js/*.js",
-        img: source_folder + "/img/**/*.+(png|jpg|gif|ico|svg|webp) "
-    },
-    clean: "./" + project_folder + "/"
-}
+  build: {
+    html: project_folder + "/",
+    css: project_folder + "/SMART-Disburse/css/",
+    js: project_folder + "/SMART-Disburse/js/",
+    img: project_folder + "/SMART-Disburse/img/",
+    fonts: project_folder + "/SMART-Disburse/fonts/",
+  },
+  src: {
+    html: [source_folder + "/views/**/*.pug", "!" + source_folder + "/views/_*.pug", "!" + source_folder + "/views/**/#**/*.pug"],
+    css: source_folder + "/stylus/*.styl",
+    js: source_folder + "/js/*.js",
+    img: source_folder + "/img/**/*.+(png|jpg|gif|ico|svg|webp)",
+    fonts: source_folder + "/fonts/*.ttf",
+  },
+  watch: {
+    html: source_folder + "/**/**/*.pug",
+    css: source_folder + "/stylus/**/*.styl",
+    js: source_folder + "/js/*.js",
+    img: source_folder + "/img/**/*.+(png|jpg|gif|ico|svg|webp) ",
+  },
+  clean: "./" + project_folder + "/",
+};
 
-let {
-    src,
-    dest
-} = require('gulp'),
-
-    gulp = require('gulp');
+let { src, dest } = require("gulp"),
+  gulp = require("gulp");
 browsersync = require("browser-sync").create();
 fileinclude = require("gulp-file-include");
 del = require("del");
@@ -43,116 +39,118 @@ imagemin = require("gulp-imagemin");
 webp = require("gulp-webp");
 webphtml = require("gulp-webp-html");
 webpcss = require("gulp-webpcss");
-pug = require('gulp-pug');
-tailwindcss = require('tailwindcss');
-tw_forms = require('@tailwindcss/forms')
-const postcss = require('gulp-postcss');
+pug = require("gulp-pug");
+tailwindcss = require("tailwindcss");
+tw_forms = require("@tailwindcss/forms");
+const postcss = require("gulp-postcss");
 
 function browserSync(params) {
-    browsersync.init({
-        server: {
-            baseDir: "./" + project_folder + "/"
-        },
-        port: 3000,
-        notify: false
-    })
+  browsersync.init({
+    server: {
+      baseDir: "./" + project_folder + "/",
+    },
+    port: 3000,
+    notify: false,
+  });
 }
 
 function html() {
-    return src(path.src.html)
-        .pipe(fileinclude())
-        //.pipe(webphtml()) // TODO: not working with old mixins and mixins in mixins
-        .pipe(pug({
-            doctype: 'html',
-            pretty: true,
-        }))
-        .pipe(dest(path.build.html))
-        .pipe(browsersync.stream())
+  return src(path.src.html)
+    .pipe(fileinclude())
+    .pipe(webphtml()) // *: working with html
+    .pipe(
+      pug({
+        doctype: "html",
+        pretty: true,
+      })
+    )
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream());
 }
 
 function images() {
-    return src(path.src.img)
-        .pipe(
-            webp({
-                quality: 70
-            })
-        )
-        .pipe(dest(path.build.img))
-        .pipe(src(path.src.img))
-        .pipe(
-            imagemin({
-                progressive: true,
-                svgoPlugins: [{
-                    removeViewBox: false
-                }],
-                interlaced: true,
-                optimizationLevel: 3 // 0 to 7
-            })
-        )
-        .pipe(dest(path.build.img))
-        .pipe(browsersync.stream())
+  return src(path.src.img)
+    .pipe(
+      webp({
+        quality: 70,
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [
+          {
+            removeViewBox: false,
+          },
+        ],
+        interlaced: true,
+        optimizationLevel: 3, // 0 to 7
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream());
 }
 
 function css() {
-    return src(path.src.css)
-        .pipe(
-            stylus({
-                // Сжатие css в dist
-                compress: false,
-            })
-        )
-        .pipe(webpcss())
-        .pipe(postcss([
-            tailwindcss('./tailwind.config.js'),
-            require('autoprefixer'),
-        ]))
-        .pipe(dest(path.build.css))
-        .pipe(clean_css())
-        .pipe(
-            rename_file({
-                extname: ".min.css"
-            })
-        )
-        .pipe(dest(path.build.css))
-        .pipe(browsersync.stream())
+  return src(path.src.css)
+    .pipe(
+      stylus({
+        // Сжатие css в dist
+        compress: false,
+      })
+    )
+    .pipe(webpcss())
+    .pipe(postcss([tailwindcss("./tailwind.config.js"), require("autoprefixer")]))
+    .pipe(dest(path.build.css))
+    .pipe(clean_css())
+    .pipe(
+      rename_file({
+        extname: ".min.css",
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream());
 }
 function js() {
-    return src(path.src.js)
-        .pipe(fileinclude())
-        .pipe(dest(path.build.js))
-        .pipe(
-            uglify()
-        )
-        .pipe(
-            rename_file({
-                extname: ".min.js"
-            })
-        )
-        .pipe(dest(path.build.js))
-        .pipe(browsersync.stream())
+  return src(path.src.js)
+    .pipe(fileinclude())
+    .pipe(dest(path.build.js))
+    .pipe(uglify())
+    .pipe(
+      rename_file({
+        extname: ".min.js",
+      })
+    )
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream());
 }
 
 function clean(params) {
-    return del(path.clean);
+  return del(path.clean);
 }
-gulp.task('svgSprite', function () {
-    return gulp.src([source_folder + '/icons/*.svg'])
-        .pipe(svgSprite({
-            mode: {
-                stack: {
-                    sprite: "../icons/icon.svg", //Название файла для спрайтов
-                    example: true
-                },
-            }
-        }))
-        .pipe(dest(path.build.img))
-})
+gulp.task("svgSprite", function () {
+  return gulp
+    .src([source_folder + "/icons/*.svg"])
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: "../icons/icon.svg", //Название файла для спрайтов
+            example: true,
+          },
+        },
+      })
+    )
+    .pipe(dest(path.build.img));
+});
 
 function watchFiles(params) {
-    gulp.watch([path.watch.html], html);
-    gulp.watch([path.watch.css], css);
-    gulp.watch([path.watch.js], js);
-    gulp.watch([path.watch.img], images);
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
+  gulp.watch([path.watch.js], js);
+  gulp.watch([path.watch.img], images);
 }
 
 let build = gulp.series(clean, gulp.parallel(html, css, js, images));
